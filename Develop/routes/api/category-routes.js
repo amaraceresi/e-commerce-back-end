cf16ab9b0dd8ac448b845830122d6eef8c9632e6
Amara Ceresi <aceresi95@gmail.com>
2023-05-22 17:17:00 -0500

    set up all create routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -35,6 +35,8 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
+    const data = await Category.create(req.body)
+    res.status(200).json(data)
   } catch (err) {
     res.status(500).json(err);
   }
